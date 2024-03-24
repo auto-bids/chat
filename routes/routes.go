9ff65552b09717a107fe244fb6a8280b9a91ae40
mati <mateusz.krz314@gmail.com>
2024-03-24 21:40:17 +0100

    changed websocket endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -9,7 +9,7 @@ import (
 func ChatRoute(router *gin.Engine, Server *server.Server) {
 	chat := router.Group("/chat")
 	{
-		chat.GET("/:email", func(ctx *gin.Context) { controllers.ManageWs(Server, ctx) })
+		chat.GET("/ws/:email", func(ctx *gin.Context) { controllers.ManageWs(Server, ctx) })
 		chat.GET("/messages/:id/:email/:page", controllers.GetMessages)
 		chat.GET("/conversations/:email", controllers.GetConversations)
 	}
